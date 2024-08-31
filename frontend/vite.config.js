3fb9675cfc5066ebc64b9dd095fb6e055c58422d
ravishankar2003 <nidumukkalaravishankar@gmail.com>
2024-08-31 17:01:19 +0530

    update config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mern-blog/', // Replace with your repository name
   plugins: [react()],
 })
