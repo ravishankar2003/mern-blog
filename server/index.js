f5fa995d25b472afaae33791c0a21b0021063d01
ravishankar2003 <nidumukkalaravishankar@gmail.com>
2025-07-23 20:39:28 +0530

    url update

--- server/index.js
+++ server/index.js
@@ -18,10 +18,7 @@ app.use(cookieParser());
 app.use(express.json())
 
 
-app.use(cors({
-  origin: 'https://ravishankar2003.github.io',
-  credentials:true
-}));
+app.use(cors());
 
 app.use(express.json({ limit: '10mb' })); // Increase body size limit if needed
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
